2f6231da2cdad4c8b370e5e61bca38be3309913f
bvn13 <mail4bvn@gmail.com>
2022-08-13 03:29:17 +0300

    BigDecimal: moving point

--- big-decimal/src/main/java/me/bvn13/test/bigdecimal/BigDecimalExample.java
+++ big-decimal/src/main/java/me/bvn13/test/bigdecimal/BigDecimalExample.java
@@ -10,10 +10,17 @@ public class BigDecimalExample {
 //        a();
 //        b();
 
-        Currency eur = Currency.getInstance("EUR");
-        System.out.println("EUR: " + eur.getDefaultFractionDigits());
-        Currency tnd = Currency.getInstance("TND");
-        System.out.println("TND:" + tnd.getDefaultFractionDigits());
+//        Currency eur = Currency.getInstance("EUR");
+//        System.out.println("EUR: " + eur.getDefaultFractionDigits());
+//        Currency tnd = Currency.getInstance("TND");
+//        System.out.println("TND:" + tnd.getDefaultFractionDigits());
+
+        BigDecimal amount = new BigDecimal("4.12");
+        System.out.println(amount);
+        amount = amount.movePointRight(2);
+        System.out.println(amount);
+        amount = amount.movePointLeft(2);
+        System.out.println(amount);
     }
 
     static void fromInteger() {
